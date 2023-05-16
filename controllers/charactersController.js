77586765815ac5c060daedb9c073f7d95fce7172
BertCasale <bthebest55@gmail.com>
2023-05-16 19:57:26 -0400

    get one character

--- controllers/charactersController.js
+++ controllers/charactersController.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const characters = express.Router();
 const {
-  getAllCharacters
+  getAllCharacters,
+  getOneCharacter,
 } = require("../queries/characters.js");
 
 characters.get("/", async (req, res) => {
@@ -13,10 +14,27 @@ characters.get("/", async (req, res) => {
     } else {
       res.status(500).json({error: "Server Error"});
     }
-
   } catch (e) {
     res.status(400).json({error: e});
   }
 });
+
+characters.get("/:id", async (req, res) => {
+  const {id} = req.params;
+
+  try {
+    const singleCharacter = await getOneCharacter(id);
+
+    if (singleCharacter.name){
+      res.status(200).json(singleCharacter)
+    } else {
+      res.status(500).json({error: "Server Error"})
+    }
+  } catch (e) {
+    res.status(400).json({error: e});
+  }
+});
+
+
 
 module.exports = characters;
--- queries/characters.js
+++ queries/characters.js
@@ -1,4 +1,4 @@
-const { all } = require("../app");
+const app = require("../app");
 const db = require("../db/dbConfig.js");
 
 const getAllCharacters = async () => {
@@ -10,6 +10,16 @@ const getAllCharacters = async () => {
   }
 }
 
+const getOneCharacter = async (id) => {
+  try {
+    const singleCharacter = await db.one("SELECT * FROM characters WHERE id=$1", id);
+    return singleCharacter;
+  } catch (e) {
+    return e;
+  }
+}
+
 module.exports = {
   getAllCharacters,
+  getOneCharacter
 }
