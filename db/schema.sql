c6ae4c9fe3a90e391c30e50caa52d5bbaabc0c62
BertCasale <bthebest55@gmail.com>
2023-05-19 09:33:11 -0400

    games table

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,18 @@ CREATE DATABASE colorful_characters_dev;
 
 \c colorful_characters_dev
 
+CREATE TABLE games (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(40) NOT NULL,
+  image TEXT NOT NULL,
+  description TEXT NOT NULL,
+  platforms TEXT,
+  release INT NOT NULL,
+  lgbt BOOLEAN DEFAULT false,
+  poc BOOLEAN DEFAULT false,
+  disability BOOLEAN DEFAULT false
+);
+
 CREATE TABLE characters (
   id SERIAL PRIMARY KEY,
   name VARCHAR(40) NOT NULL,
@@ -17,5 +29,6 @@ CREATE TABLE characters (
   poc_type VARCHAR(50),
   disability BOOLEAN DEFAULT false,
   disability_type VARCHAR(50),
-  game VARCHAR(40) NOT NULL
+  game_id INTEGER REFERENCES games(id) ON DELETE CASCADE
 );
+
